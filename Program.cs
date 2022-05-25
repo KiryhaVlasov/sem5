81db9e2a1db5b9419463d3bf6c179804c99e48cd
KiryhaVlasov <103191525+KiryhaVlasov@users.noreply.github.com>
2022-05-25 21:20:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, стоящих 
+// на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+Console.Clear();
+int sum = 0;
+int[] array = new int[4];
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 100);
+
+    Console.Write($" {array[i]} ");
+
+    if (i % 2 == 1)
+    {
+
+        sum = sum + array[i];
+
+    }
+
+
+}
+Console.Write($"] -> {sum}");
